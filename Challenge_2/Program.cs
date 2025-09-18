4a07851fe4551e1aa86b21b987e99094e2958dcd
Kingkonbro <konradboruch735@gmail.com>
2025-09-18 17:25:52 +0100

    Completed

---
+++ Challenge_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The forename name and surname is asked of the user
+            Console.Write("Enter your forename: ");
+            string userForename = Console.ReadLine();
+            Console.Write("Enter your surname: ");
+            string userSurname = Console.ReadLine();
+
+            char initial = userForename[0];
+            string fullname = (initial + " " + userSurname).ToUpper();
+
+            // The variable 'fullname' is printed out the user
+            Console.Write($"Fullname: {fullname}");
+
+        }
+    }
+}
