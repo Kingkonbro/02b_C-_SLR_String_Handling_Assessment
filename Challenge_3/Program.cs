bac4ff328c999a9b278b56139b8b34fcfbe71194
Kingkonbro <konradboruch735@gmail.com>
2025-09-18 17:34:13 +0100

    Completed

---
+++ Challenge_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The user is prompted to enter the name of two citys
+            Console.WriteLine("Please input two city names, one after the other:");
+            string cityOne = Console.ReadLine();
+            string cityTwo = Console.ReadLine();
+            
+            // The strings are manipulated for the output
+            string result = (cityOne.Substring(0,4) + "-" + cityTwo.Substring(0,4)).ToUpper();
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
